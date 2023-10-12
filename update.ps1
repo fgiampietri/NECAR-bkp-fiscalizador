073ff741a9a5aeb85d21a387271eb27242d68815
Federico Giampietri <58990965+fgiampietri@users.noreply.github.com>
2023-10-12 19:36:21 -0300

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -8,4 +8,4 @@ Start-Sleep -Seconds 3
 
 Write-Output "[*] Done!"
 Write-Output "`nUpdates in 2.0 - Word support "
-Read-Host "`nPress any key to continue. If errors, grab a screenshot and submit an issue with the debug log on github, otherwise run the new version of Backup Fiscalizador. !
+Read-Host "`nPress any key to continue. If errors, grab a screenshot and submit an issue with the debug log on github, otherwise run the new version of Backup Fiscalizador. !"
